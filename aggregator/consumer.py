691ac0d57f66ab1973f20d250e7e836ee978b024
Lekealem Asong <lekealem.asong@freeyou.ag>
2024-03-21 14:23:54 +0100

    Modify consumer to be able to scale and consider checkpointing and state management

--- aggregator/consumer.py
+++ aggregator/consumer.py
@@ -1,29 +1,40 @@
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer, OffsetAndMetadata, TopicPartition
 import time
 import json
 
 class Consumer:
-    def __init__(self, server: str, topic: str, group_id: str, chunk_size: int) -> None:
+    def __init__(self, server: str, topic: str) -> None:
+        self.topic = topic
+        self.chunk_size = 50
+        self.chunk_timeout = 5000 # in miliseconds
+        self.last_message = None
+
         self.consumer = KafkaConsumer(
-            topic,
-            group_id=group_id,
+            self.topic,
+            group_id='data-aggregator',
             bootstrap_servers=[server],
             auto_offset_reset='earliest',  # Start reading at the earliest message
             enable_auto_commit=False,  # We'll manually commit offsets
+            value_deserializer=lambda mes: json.loads(mes.decode('utf-8')),
+            max_poll_records=self.chunk_size
         )
-        self.chunk_size = chunk_size
-        self.chunk_timeout = 5  # Time in seconds to wait for filling the chunk
 
     def messages_chunk(self) -> list[dict]:
-        messages = []
-        start_time = time.time()
+        batch = self.consumer.poll(timeout_ms=self.chunk_timeout)
 
-        for message in self.consumer:
-            messages.append(json.loads(message.value))
+        # Flatten the batch into a list of messages
+        messages = [msg for _, msgs in batch.items() for msg in msgs]
 
-            # Check if chunk is full or timeout has occurred
-            if len(messages) >= self.chunk_size or (time.time() - start_time) >= self.chunk_timeout:
-                self.consumer.commit()
-                break
+        if messages:
+            self.last_message = messages[-1]
 
-        return messages
+        return [msg.value for msg in messages]
+
+    def commit(self) -> None:
+        if self.last_message:
+            last_topic_partition = TopicPartition(self.last_message.topic, self.last_message.partition)
+            last_offset_and_metadata = OffsetAndMetadata(self.last_message.offset + 1, None)
+            self.consumer.commit({ last_topic_partition: last_offset_and_metadata })
+
+    def close(self) -> None:
+        self.consumer.close()
--- aggregator/main.py
+++ aggregator/main.py
@@ -3,8 +3,11 @@ from consumer import Consumer
 from data_aggregator import DataAggregator
 
 if __name__ == "__main__":
-    kafka_consumer = Consumer(server='localhost:9094', topic='data', group_id='my-personal-group', chunk_size=50)
+    kafka_consumer = Consumer(server='localhost:9094', topic='data')
     aggregator = DataAggregator(kafka_consumer.messages_chunk())
 
     for message in aggregator.aggregated_data():
         print(message)
+
+    kafka_consumer.commit()
+    kafka_consumer.close()
